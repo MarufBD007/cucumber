a08af86191a997fa079d1c8f3c454ee13040efd1
Maruf Rahman <marufr@springrainit.com>
2020-01-30 13:40:14 +0600

    updated

--- features/login.feature
+++ features/login.feature
@@ -1,7 +1,14 @@
 Feature: User Login
-    Scenario: Login with valid Scenario
+    Scenario: Login with valid credentials
         Given I am on the login page
         When I fill in Email with "marufr@springrainit.com"
         And I fill in Password with "asdzxc"
         And I press Login
-        Then I should get status 200 and success status along with a valid token
+        Then I should get status 200 and status "success" along with a valid token
+
+    Scenario: Login with invalid credentials
+        Given I am on the login page
+        When I fill in Email with "marufr1@springrainit.com"
+        And I fill in Password with "asdzxc1"
+        And I press Login
+        Then I should get status 401 and status fail
--- steps/login.js
+++ steps/login.js
@@ -1,25 +1,28 @@
 const { Given, Then, When } = require('cucumber');
 const assert = require('assert');
 const { publicGet, publicPost} = require("../api");
-
-(async() => {
-    request = {};
-    responseData = {};
-    Given('I am on the login page', function () { });
-    When('I fill in Email with {string}', function (input) {
-        request.email = input;
+let request = {};
+let responseData = {};
+Given('I am on the login page', function () { });
+When('I fill in Email with {string}', function (input) {
+    request.email = input;
+});
+When('I fill in Password with {string}', function (input) {
+    request.password = input;
+});
+When('I press Login', () => {
+    return publicPost('https://api.dev.auws.cloud/auth/login',request).then(response => {
+        responseData = response;
+    }).catch(error=>{
+        responseData = error.response;
     });
-    When('I fill in Password with {string}', function (input) {
-        request.password = input;
-    });
-    When('I press Login', () => {
-        return publicPost('https://api.dev.auws.cloud/auth/login',request).then(response => {
-            responseData = response;
-        });
-    });
-    Then('I should get status {int} and success status along with a valid token', function (input) {
-        assert(responseData.status === input);
-        assert(responseData.data.status === "success");
-        assert(responseData.data.token !== "");
-    });
-})();
+});
+Then('I should get status {int} and status {string} along with a valid token', function (input, input2) {
+    assert(responseData.status === input);
+    assert(responseData.data.status === input2);
+    assert(responseData.data.token !== "");
+});
+Then('I should get status {int} and status fail', function (input) {
+    assert(responseData.status === input);
+    assert(responseData.data.status === "fail");
+});
