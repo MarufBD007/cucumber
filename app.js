3161a57baefc502ce9c877ff020d8784c8b0620f
Maruf Rahman <marufrahmanbd08@gmail.com>
2020-02-06 21:01:13 +0600

    updated

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express')
+const http = require('http');
+const app = express()
+
+app.get('/', async (req, res) => {
+    res.send({ status: 'success' })
+})
+
+http.createServer(app).listen(process.env.PORT || 8000)
