6cc5bbf0e11b70f4338f8dc8f284e528e9f5f6f8
Maruf Rahman <marufrahmanbd08@gmail.com>
2020-02-06 19:03:33 +0600

    test code pipeline

--- steps/account.js
+++ steps/account.js
@@ -1,18 +1,19 @@
 const { Then, When } = require('cucumber');
 const { baseUrl } = require("../config");
 const assert = require('assert');
-const { privateGet} = require("./apiCaller");
+const { privateGet } = require("./apiCaller");
 let responseData = {};
 
-When('I call the api with Bearer token for fetching account list', function(){
-    return  privateGet(`${baseUrl}/accounts/list`, this.token).then(response => {
+
+When('I call the api with Bearer token for fetching account list', function () {
+    return privateGet(`${baseUrl}/accounts/list`, this.token).then(response => {
         responseData = response;
     }).catch(error => {
         responseData = error.response;
-    }); 
+    });
 });
 
-Then('I should see the list of accounts', function(){
+Then('I should see the list of accounts', function () {
     assert(responseData.data.status === "success");
     assert(responseData.data.data.length > 0);
 });
