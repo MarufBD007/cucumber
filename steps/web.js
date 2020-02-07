9b534a72efe2c2e3f4aef64daea493485db900cc
Maruf Rahman <marufrahmanbd08@gmail.com>
2020-02-07 20:30:34 +0600

    updated

--- steps/web.js
+++ steps/web.js
@@ -1,11 +1,9 @@
-// const chromedriver = require('chromedriver');
 const { Given, Then, When, AfterAll, setDefaultTimeout } = require('cucumber');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const assert = require('assert');
 const sleep = require("sleep");
 let driver = null;
-// chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 const screen = { width: 1700, height: 900 };
 setDefaultTimeout(80 * 1000);
 Given('I am on the webportal login page', function () {
@@ -21,9 +19,9 @@ Then('I should see dashboard page', async function (data) {
     sleep.sleep(5);
     const url = await driver.getCurrentUrl();
     assert(url === data.raw()[1][1]);
- });
-AfterAll(async function() {
-    if(driver){
+});
+AfterAll(async function () {
+    if (driver) {
         return await driver.quit();
     }
 });
